fe19e05bc4362b1bc96ea18840847ec7676c389b
unknown <raulheri43@hotmail.com>
2022-04-30 21:41:21 -0300

    Updates

---
+++ precache-manifest.9a09127571b3f8000f794f80e099216b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "823ea1b91d6509d77f8e",
+    "url": "/raulhenrique-x3/GitHub-Repositore-API----ReactJS/static/css/main.ae261ada.chunk.css"
+  },
+  {
+    "revision": "823ea1b91d6509d77f8e",
+    "url": "/raulhenrique-x3/GitHub-Repositore-API----ReactJS/static/js/main.823ea1b9.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/raulhenrique-x3/GitHub-Repositore-API----ReactJS/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "1af1368c091066cd9791",
+    "url": "/raulhenrique-x3/GitHub-Repositore-API----ReactJS/static/js/2.1af1368c.chunk.js"
+  },
+  {
+    "revision": "aea94f015d0da409942b",
+    "url": "/raulhenrique-x3/GitHub-Repositore-API----ReactJS/static/js/runtime~main.aea94f01.js"
+  },
+  {
+    "revision": "1e21d44e5a4aa1dbb648b6f781791f47",
+    "url": "/raulhenrique-x3/GitHub-Repositore-API----ReactJS/index.html"
+  }
+];
